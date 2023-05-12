68799368e8746423a24174d22a2603dfe9f51988
DV-555 <valentin.dumler@gmail.com>
2023-05-12 11:39:41 +0200

    feat class Superman

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        SuperMan superMan1 = new SuperMan();
+        superMan1.fly();
+        superMan1.run();
+        superMan1.swim();
     }
+
+
+
 }
---
+++ src/SuperMan.java
@@ -0,0 +1,20 @@
+public class SuperMan implements CanFly, CanRun, CanSwim {
+
+
+    @Override
+    public void fly() {
+        System.out.println("Superman can fly");
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Superman can run");
+
+    }
+
+    @Override
+    public void swim() {
+        System.out.println("Superman can swim");
+
+    }
+}
